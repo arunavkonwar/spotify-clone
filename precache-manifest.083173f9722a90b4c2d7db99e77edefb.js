0323a6c005e6e5ea1f58632ae202018ac21ce999
Arunav Konwar <arunavkonwar@gmail.com>
2019-12-26 19:28:49 +0530

    Updates

---
+++ precache-manifest.083173f9722a90b4c2d7db99e77edefb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1f3fe8f58381950bd37704d11aa760",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f9ec7127bb28e2fc7b5f",
+    "url": "/my-app/static/css/main.91c0b5f0.chunk.css"
+  },
+  {
+    "revision": "8601e42425321be6868a",
+    "url": "/my-app/static/js/2.62f394ae.chunk.js"
+  },
+  {
+    "revision": "f9ec7127bb28e2fc7b5f",
+    "url": "/my-app/static/js/main.f6356377.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
