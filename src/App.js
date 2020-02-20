e6f50b095154a6c8ff4f142c34d128df286fc572
Arunav Konwar <arunavkonwar@gmail.com>
2020-02-20 06:55:54 +0530

    fixed login page

--- src/App.js
+++ src/App.js
@@ -13,55 +13,13 @@ class App extends Component {
     super(props);
 
     this.state = {
-      user: null,
-      token: null,
-      item: {
-        album: {
-          images: [{ url: "" }]
-        },
-        name: "",
-        artists: [{ name: "" }],
-        duration_ms:0,
-      },
-      is_playing: "Paused",
-      progress_ms: 0
+      user: null
     };
-    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
   }
 
   componentDidMount(){
-    //this.authListener();
-    // Set token
-    let _token = hash.access_token;
-
-    if (_token) {
-      // Set token
-      this.setState({
-        token: _token
-      });
-      this.getCurrentlyPlaying(_token);
-    }
+    this.authListener();
   }
-
-  getCurrentlyPlaying(token) {
-    // Make a call using the token
-    $.ajax({
-      url: "https://api.spotify.com/v1/me/player",
-      type: "GET",
-      beforeSend: (xhr) => {
-        xhr.setRequestHeader("Authorization", "Bearer " + token);
-      },
-      success: (data) => {
-        console.log("data", data);
-        this.setState({
-          item: data.item,
-          is_playing: data.is_playing,
-          progress_ms: data.progress_ms,
-        });
-      }
-    });
-  }
-
 
   authListener(){
     fire.auth().onAuthStateChanged((user)=>{
@@ -77,14 +35,7 @@ class App extends Component {
   render(){
     return(
       <div className="App">
-        {this.state.user ? (<Home1/>):(<Home1/>)}
-        {this.state.token && (
-            <Player
-              item={this.state.item}
-              is_playing={this.state.is_playing}
-              progress_ms={this.progress_ms}
-            />
-        )}          
+        {this.state.user ? (<Home1/>):(<Login/>)}        
       </div>
     );
   }
