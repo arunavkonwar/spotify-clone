ffd5d95052b1f643236dedbad0ea96ac6538b233
Arunav Konwar <arunavkonwar@gmail.com>
2020-02-20 11:04:58 +0530

    flexbox not working

--- src/components/Playbar.js
+++ src/components/Playbar.js
@@ -6,14 +6,34 @@ import SongName from './SongName'
 
 class Playbar extends Component {
 
+  reverseTitleLookup = (e) => {
+    for (var key in this.props.rawData) {
+      if (this.props.rawData[key]['id'] === e){
+          return(this.props.rawData[key]['title']) 
+      }
+    }
+  }
+
+  reverseArtistLookup = (e) => {
+    for (var key in this.props.rawData) {
+      if (this.props.rawData[key]['id'] === e){
+          return(this.props.rawData[key]['artist']) 
+      }
+    }
+  }
+
   render(){
       return(
         <div>
           <div className="playbar">
-              {/* <div className="playbar-songName"> */}
+              <div className="playbar-songName">
               {/* <SongName rawData={this.props.rawData} songPlaying={this.props.songPlaying} id={this.props.songPlaying}/> */}
-              {/* </div> */}
+              {/* {this.reverseTitleLookup(this.props.songPlaying)}<br></br>
+              {this.reverseArtistLookup(this.props.songPlaying)} */}
+              </div>
+              <div className="audio-player-center">
               <Audioplayer src={this.props.src} storage={this.props.storage}/>
+              </div>
           </div>
         </div>
       );
